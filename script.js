44dce2edc253b4f4ebb8964b37b88af9e54c2a18
Maureen VOGEL <maureenvogel@Maureen-VOGELs-MacBook-Pro.local>
2016-02-22 14:48:29 -0500

    Got deck to shuffle using Fisher-Yates method

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
 cardArray=["A","A","B","B","C","C","D","D","E","E","F","F","G","G","H","H"];
 
+//// SHUFFLE DECK
+var shuffleDeck=function(){
+   // Using the Fisher-Yates (Knuth) shuffle
+    var currentIndex = cardArray.length
+      , temporaryValue
+      , randomIndex;
+
+   // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = cardArray[currentIndex];
+      cardArray[currentIndex] = cardArray[randomIndex];
+      cardArray[randomIndex] = temporaryValue;
+    }
+  }
+
+  shuffleDeck();
 
 var board=document.getElementById("board");
 
 for(var i=0; i<cardArray.length; i++){
-// appending cards
+
+// appending cards to board
   var cardsBack=document.createElement("div");
   cardsBack.classList.add("cardsBack");
   board.appendChild(cardsBack);
+
 // appending letters to cards
   var letter=document.createElement("p");
   var a=document.createTextNode(cardArray[i]);
@@ -21,4 +44,5 @@ cardsBack.addEventListener("click",function(){
     this.classList.toggle("cardsFront");
 
     })
-}
+
+} // close for loop
