4b2cd8290fbfa8afa4ff34706a670c50f1410685
Maureen VOGEL <maureenvogel@Maureen-VOGELs-MacBook-Pro.local>
2016-02-22 11:59:17 -0500

    Cards will flip over to reveal text

--- script.js
+++ script.js
@@ -3,13 +3,22 @@ cardArray=["A","A","B","B","C","C","D","D","E","E","F","F","G","G","H","H"];
 
 var board=document.getElementById("board");
 
-for(var i=0; i<16; i++){
-  var cards=document.createElement("div");
-  cards.classList.add("cards");
-  board.appendChild(cards);
+for(var i=0; i<cardArray.length; i++){
+// appending cards
+  var cardsBack=document.createElement("div");
+  cardsBack.classList.add("cardsBack");
+  board.appendChild(cardsBack);
+// appending letters to cards
+  var letter=document.createElement("p");
+  var a=document.createTextNode("A");
+  letter.appendChild(a);
+  letter.classList.add("textstyle"); //add css styling to text
+  cardsBack.appendChild(letter);
+
 
 // Change color of cards on click
-  cards.addEventListener("click",function(){
-    this.style.backgroundColor="blue";
-  })
+cardsBack.addEventListener("click",function(){
+    this.classList.toggle("cardsFront");
+
+    })
 }
