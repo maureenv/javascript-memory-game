ecda30244a0c54f7e990bac8c2fa84dd61d781cd
Maureen VOGEL <maureenvogel@Maureen-VOGELs-MacBook-Pro.local>
2016-02-22 10:49:18 -0500

    Cards on board change color on click

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-document.write("Hello is this working?");
+cardArray=["A","A","B","B","C","C","D","D","E","E","F","F","G","G","H","H"];
+
+
+var board=document.getElementById("board");
+
+for(var i=0; i<16; i++){
+  var cards=document.createElement("div");
+  cards.classList.add("cards");
+  board.appendChild(cards);
+
+// Change color of cards on click
+  cards.addEventListener("click",function(){
+    this.style.backgroundColor="blue";
+  })
+}
