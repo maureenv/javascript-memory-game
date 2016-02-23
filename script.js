aac7aa5e4c3987a6cdb951e8de410e296491a72d
Maureen VOGEL <maureenvogel@Maureen-VOGELs-MacBook-Pro.local>
2016-02-23 16:31:53 -0500

    The cards will flip back after one second if they do not match

--- script.js
+++ script.js
@@ -4,23 +4,23 @@ clickedCard=[];
 
 ///////////////////////////////////////// SHUFFLE DECK
 var shuffleDeck=function(){
-   // Using the Fisher-Yates (Knuth) shuffle
-    var currentIndex = cardArray.length
-      , temporaryValue
-      , randomIndex;
+  // Using the Fisher-Yates (Knuth) shuffle
+  var currentIndex = cardArray.length
+  , temporaryValue
+  , randomIndex;
 
-   // While there remain elements to shuffle...
-    while (0 !== currentIndex) {
-      // Pick a remaining element...
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
 
-      // And swap it with the current element.
-      temporaryValue = cardArray[currentIndex];
-      cardArray[currentIndex] = cardArray[randomIndex];
-      cardArray[randomIndex] = temporaryValue;
-    }
+    // And swap it with the current element.
+    temporaryValue = cardArray[currentIndex];
+    cardArray[currentIndex] = cardArray[randomIndex];
+    cardArray[randomIndex] = temporaryValue;
   }
+}
 ///////////////////////// End shuffle DECK
 
 shuffleDeck();
@@ -29,12 +29,12 @@ var board=document.getElementById("board");
 
 for(var i=0; i<cardArray.length; i++){
 
-// appending cards to board
+  // appending cards to board
   var cardsBack=document.createElement("div");
-  cardsBack.classList.add("cardsBack");
+  cardsBack.classList.add("card");
   board.appendChild(cardsBack);
 
-// appending letters to cards
+  // appending letters to cards
   var letter=document.createElement("p");
   var arrayNumber=document.createTextNode(cardArray[i]);
   letter.appendChild(arrayNumber);
@@ -44,35 +44,30 @@ for(var i=0; i<cardArray.length; i++){
   // data-deck-position finder
   cardsBack.setAttribute("data-deck-position", i);
 
-    if (clickedCard.length<2){
     // Change color of cards on click
-      cardsBack.addEventListener("click",function(){
-          this.classList.toggle("cardsFront");
-          game(this.getAttribute("data-deck-position"));
-            clickedCard.push(cardLetter);
-              console.log(clickedCard);
-              compare();
-      })
+  cardsBack.addEventListener("click",function(){
+    var card = this;
+    if (clickedCard.length < 2){
+      card.classList.add("front");
+      // var cardLetter = game(this.getAttribute("data-deck-position"));
+      clickedCard.push(card.textContent);
+  
+      if (clickedCard.length === 2){
+        if (clickedCard[0] === clickedCard[1]){
+          console.log("It's a match!");
+        } else {
+          console.log("It's not a match!")
+          setTimeout(turnOffAllCards, 1000);
+        }
+      }
     }
+  })
 } // close for loop
 
-///// Get letter of the card that is clicked on
-var cardLetter;
-var game=function(x){
-  value=document.getElementsByClassName("cardsBack");
-  cardLetter = value[x].textContent; // this returns only the text on the card
-}
+function turnOffAllCards(){
+  var frontCards = document.querySelectorAll(".front");
+  for(var i = 0; i < frontCards.length; i++){
+    frontCards[i].classList.remove("front");
 
-////Compare clicked cards
-
-var compare=function(){
-  if (clickedCard.length===2){
-    if (clickedCard[0]===clickedCard[1]){
-
-      console.log("It's a match");
-    } else {
-      //console.log("It's not a match");
-      clickedCard.classList.remove("cardsFront");
-    }
   }
 }
